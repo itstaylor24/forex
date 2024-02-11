3ae5490c1d9e7adfe678182bc9e0dcac34eb2c00
Taylor Harris <itstaylor24@yahoo.com>
2024-02-10 19:57:32 -0700

    modified app.py

--- Forex-converter/app.py
+++ Forex-converter/app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, flash 
+from flask import Flask, render_template, request, flash,session, redirect
 import requests
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "chicken123"
+
 
 
 @app.route('/')
@@ -13,24 +15,38 @@ def submit_form():
     """Handles form submission"""
     currencyFrom = request.form['from'].upper()
     currencyTo = request.form['to'].upper()
-    Amount = float(request.form['amount'])
+    Amount = float(request.form.get['amount'])
+    # if not Amount:
+    #    flash ("must enter numerical value")
+    #    return redirect('/')
     
-    url = requests.get(f'http://api.exchangerate.host/convert?access_key=0b39e76fdf15c90ece4e1736d13383cd&from={currencyFrom}&to={currencyTo}&amount={Amount}')
+    
+    
     valid_currency_codes = requests.get('http://api.exchangerate.host/list?access_key=0b39e76fdf15c90ece4e1736d13383cd')
     all_currencies = valid_currency_codes.json()['currencies']
-    if currencyFrom not in all_currencies:
-      flash("'from' currency not in database")
+    if len(currencyFrom) == 0 or len(currencyTo) == 0:
+       flash("must input data in all spaces")
+       return redirect('/')
+    
+    elif currencyFrom not in all_currencies:
+       flash("'from' currency not in database")
+       return redirect('/')
     #  raise KeyError(" 'from' currency not in database")
     elif currencyTo not in all_currencies:
-       flash(" 'to' currency not in database")
+        flash(" 'to' currency not in database")
+        return redirect('/')
+
     #  raise KeyError(" 'to' currency not in database")
-    elif Amount is not isinstance(Amount, (float,int)):
-      flash("must enter a proper numerical amount")
-     
+    elif not isinstance(Amount, float):
+       flash("must enter a proper numerical amount")
+       return redirect('/')
+
+      # raise ValueError("must enter a proper numerical amount")
 
     else: 
-     Result = url.json()['result']
-     roundedResult = round(Result,2)
+     url = requests.get(f'http://api.exchangerate.host/convert?access_key=0b39e76fdf15c90ece4e1736d13383cd&from={currencyFrom}&to={currencyTo}&amount={Amount}')
+    Result = url.json()['result']
+    roundedResult = round(Result,2)
     return render_template('currency_form.html', currency_from=currencyFrom, currency_to=currencyTo, amount=Amount, conversion_result=roundedResult)
     
  
